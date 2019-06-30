c70ca19773ec06cf4c190e429e87819377ef0a0a
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-06-30 19:23:35 +0100

    correct code quality issues

--- test/transaction-log-spec.js
+++ test/transaction-log-spec.js
@@ -1,23 +1,23 @@
-var assert = require('assert');
-var TransactionLog = require('../src/transaction-log');
+var assert = require('assert')
+var TransactionLog = require('../src/transaction-log')
 
-describe('TransactionLog', function() {
-  describe('#add', function() {
-    it('adds a new transaction to the log', function() {
-      let dateNow = new Date();
+describe('TransactionLog', function () {
+  describe('#add', function () {
+    it('adds a new transaction to the log', function () {
+      let dateNow = new Date()
       let transaction = {
         'date': dateNow,
         'amount': 5,
         'balance': 5
       }
-      var sut = new TransactionLog();
+      var sut = new TransactionLog()
 
-      sut.add(transaction);
+      sut.add(transaction)
 
-      assert.deepEqual(sut.history, [transaction]);
-      assert.equal(sut.history[0].date, dateNow);
-      assert.equal(sut.history[0].amount, 5);
-      assert.equal(sut.history[0].balance, 5);
-    });
-  });
-});
+      assert.deepStrictEqual(sut.history, [transaction])
+      assert.strictEqual(sut.history[0].date, dateNow)
+      assert.strictEqual(sut.history[0].amount, 5)
+      assert.strictEqual(sut.history[0].balance, 5)
+    })
+  })
+})
