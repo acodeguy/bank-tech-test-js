c6593a3123f6c9dffa099211ea4d801807849583
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-06-30 19:21:44 +0100

    correct code quality issues

--- test/statement-spec.js
+++ test/statement-spec.js
@@ -1,28 +1,27 @@
-var assert = require('assert');
-var Statement = require('../src/statement');
+var assert = require('assert')
+var Statement = require('../src/statement')
 
-describe('Statement', function() {
-  describe('#print', function() {
-    it('prints the statement to the screen', function() {
-      
+describe('Statement', function () {
+  describe('#print', function () {
+    it('prints the statement to the screen', function () {
       var transactionLogData = [{
         'date': new Date(),
         'amount': 10,
         'balance': 10
-        },
-        {
-          'date': new Date(),
-          'amount': -5,
-          'balance': 5
-        }
-    ];
-      var sut = new Statement(transactionLogData);
-      let dateTodayFormatted = Statement.getShortDate(new Date());
-      var expectedStatement = 'date || credit || debit || balance' + "\n" +
-        dateTodayFormatted + ' || ' + '10.00 || || 10.00' + "\n" +
-        dateTodayFormatted + ' || || 5.00 || 5.00\n';
+      },
+      {
+        'date': new Date(),
+        'amount': -5,
+        'balance': 5
+      }
+      ]
+      var sut = new Statement(transactionLogData)
+      let dateTodayFormatted = Statement.getShortDate(new Date())
+      var expectedStatement = 'date || credit || debit || balance\n' +
+        dateTodayFormatted + ' || ' + '10.00 || || 10.00\n' +
+        dateTodayFormatted + ' || || 5.00 || 5.00\n'
 
-      assert.equal(sut.print(), expectedStatement);
-    });
-  });
-});
+      assert.strictEqual(sut.print(), expectedStatement)
+    })
+  })
+})
