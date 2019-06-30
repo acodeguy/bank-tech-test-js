54328bed3d370c1b6592525afa8ac55fe3d8df75
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-06-30 19:08:12 +0100

    correct code quality issues

--- src/account.js
+++ src/account.js
@@ -1,24 +1,24 @@
-var TransactionLog = require('../src/transaction-log');
+var TransactionLog = require('../src/transaction-log')
 
 class Account {
-  constructor(transactionLog = new TransactionLog()) {
-    this.balance = 0;
-    this.transactionLog = transactionLog;
+  constructor (transactionLog = new TransactionLog()) {
+    this.balance = 0
+    this.transactionLog = transactionLog
   }
 
-  transact(amount) {
-    if(typeof amount != 'number') {
-      throw new Error('Only numbers accepted.');
+  transact (amount) {
+    if (typeof amount !== 'number') {
+      throw new Error('Only numbers accepted.')
     } else {
-      this.balance += amount;
+      this.balance += amount
       let transaction = {
         'date': new Date(),
         'amount': amount,
         'balance': this.balance
       }
-      this.transactionLog.add(transaction);
+      this.transactionLog.add(transaction)
     }
   }
 }
 
-module.exports = exports = Account;
+module.exports = exports = Account
