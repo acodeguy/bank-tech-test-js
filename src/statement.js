3c476b0fd0a5819c095110e679e9ac3ef1779a4c
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-06-30 19:09:34 +0100

    correct code quality issues

--- src/statement.js
+++ src/statement.js
@@ -1,30 +1,30 @@
 class Statement {
-  constructor(transactionLogData) {
-    this.transactionLog = transactionLogData;
+  constructor (transactionLogData) {
+    this.transactionLog = transactionLogData
   }
 
-  print() {
-    var statement = 'date || credit || debit || balance' + "\n";
+  print () {
+    var statement = 'date || credit || debit || balance\n'
 
     this.transactionLog.forEach(transaction => {
-      statement += Statement.getShortDate(transaction.date);
-      if(transaction.amount > 0) statement += ' || ' + Statement.formatCurrency(transaction.amount) + ' ||';
-      if(transaction.amount < 0) statement += ' || || ' + Statement.formatCurrency(transaction.amount);
-      statement += ' || ' + Statement.formatCurrency(transaction.balance) + '\n';
-    });
-    return statement;
+      statement += Statement.getShortDate(transaction.date)
+      if (transaction.amount > 0) statement += ' || ' + Statement.formatCurrency(transaction.amount) + ' ||'
+      if (transaction.amount < 0) statement += ' || || ' + Statement.formatCurrency(transaction.amount)
+      statement += ' || ' + Statement.formatCurrency(transaction.balance) + '\n'
+    })
+    return statement
   }
 
-  static getShortDate(dateObj) {
-    let date = dateObj.getDate();
-    let month = dateObj.getMonth() + 1;
-    let year = dateObj.getFullYear();
+  static getShortDate (dateObj) {
+    let date = dateObj.getDate()
+    let month = dateObj.getMonth() + 1
+    let year = dateObj.getFullYear()
     return date + '/' + month + '/' + year
   }
 
-  static formatCurrency(amount) {
-    return amount.toFixed(2).replace('-','');
+  static formatCurrency (amount) {
+    return amount.toFixed(2).replace('-', '')
   }
 }
 
-module.exports = Statement;
+module.exports = Statement
